d3f45e9350b5ff82ddcf547473008cd4ae35038e
Aaron S. Crandall <crandall@gonzaga.edu>
2022-08-30 11:39:34 -0700

    Adding more examples

---
+++ simpleCout/simpleCout.cpp
@@ -0,0 +1,10 @@
+
+// This adds in a "library" of pre-built code for you
+// In this case, it gives your program the ability to print to the terminal
+// And it can read in things from the user, but we're not doing that here yet
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World";
+}
+
---
+++ simpleIntMain/simpleIntMain.cpp
@@ -0,0 +1,6 @@
+
+
+int main() {
+	// why int and not integer? laziness and storage space sizes in 1970
+	int myNumber = 5;
+}
