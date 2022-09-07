4026b81794815664c40cdaa0a480d076360b62d2
Aaron S. Crandall <crandall@gonzaga.edu>
2022-09-07 10:42:58 -0700

    Added start of int to float comparisions

---
+++ intVsFloat/intVsFloat.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+	int storeInt = 8;
+	float storeFloat = 5.3;
+
+	std::cout << "Integer: " << storeInt << std::endl;
+	std::cout << "Float: " << storeFloat << std::endl;
+
+	storeInt = storeFloat;
+	std::cout << "Float in Int: " << storeInt << std::endl;
+
+}
