6130990ced217a98797e32504275da85051d5d92
Aaron S. Crandall <crandall@gonzaga.edu>
2022-09-09 10:42:53 -0700

    Initial receipt gen start

---
+++ receiptGenerator/genReceipt.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Crandall's Receipt Generator
+ */
+
+#include <iostream>
+
+
+int main() {
+    float appleCost = 3.9;
+    float orangeCost = 2.5;
+    float milkCost = 3.8;
+
+    float subtotal = 0.0;
+    float total = 0.0;
+    float taxRate = 8.3;
+
+
+}
