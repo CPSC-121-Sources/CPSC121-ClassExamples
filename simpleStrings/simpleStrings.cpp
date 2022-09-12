c51e723bddb51ac4a093bc724b942a1cf82e6c87
Aaron S. Crandall <crandall@gonzaga.edu>
2022-09-12 10:50:02 -0700

    Added initial simple string examples

---
+++ simpleStrings/simpleStrings.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+
+
+int main() {
+    std::string strA = "Hello";
+    std::string strB = "World";
+    std::string strOut = strA + strB;
+
+    std::cout << strOut << std::endl;
+}
