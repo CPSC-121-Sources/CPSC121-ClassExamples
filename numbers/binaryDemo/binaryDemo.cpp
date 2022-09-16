01a8b14efce82762d4444bdf0f14ff51df809e0b
Aaron S. Crandall <acrandal@gmail.com>
2022-09-16 12:03:03 -0700

    Binary demo output

--- numbers/binaryDemo/binaryDemo.cpp
+++ numbers/binaryDemo/binaryDemo.cpp
@@ -3,9 +3,12 @@
 #include <bitset>
 
 int main() {
-    int binNumber = 0b00000001;
+    int binNumber = 0b00000000000000000000000000000011;
+    int binNumber2 = 0;
 
     std::cout << binNumber << std::endl;
-    std::cout << std::bitset<8>(binNumber) << std::endl;
+    std::cout << std::bitset<32>(binNumber) << std::endl;
+    std::cout << &binNumber << std::endl;
+    std::cout << &binNumber2 << std::endl;
 
 }
