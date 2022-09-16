b807862aa1044c2444aef987198a56e12d53d441
Aaron S. Crandall <crandall@gonzaga.edu>
2022-09-16 10:56:39 -0700

    Found how to send out binary>

--- numbers/binaryDemo/binaryDemo.cpp
+++ numbers/binaryDemo/binaryDemo.cpp
@@ -1,9 +1,11 @@
 
 #include <iostream>
+#include <bitset>
 
 int main() {
     int binNumber = 0b00000001;
 
     std::cout << binNumber << std::endl;
+    std::cout << std::bitset<8>(binNumber) << std::endl;
 
 }
