1f793c709f614ccfa0cf1e90b25ae1e027c57893
Aaron S. Crandall <acrandal@gmail.com>
2022-09-16 10:33:42 -0700

    Misc class hacking

--- simpleStrings/simpleStrings.cpp
+++ simpleStrings/simpleStrings.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::string strA = "Hello";
     std::string strB = "World";
-    std::string strOut = strA + strB;
+    std::string strOut = strA + " " + strB;
 
     std::cout << strOut << std::endl;
 }
