f40751ff53aa79945c51aa0a332d82cbc4f43fae
Aaron S. Crandall <crandall@gonzaga.edu>
2022-11-11 10:42:16 -0800

    Added very simple 3 file program example

---
+++ 3FilePrograms/first3FileDemo/header.cpp
@@ -0,0 +1,6 @@
+
+#include "header.h"
+
+void myFuncPrints() {
+    cout << "this is the function printing" << endl;
+}
---
+++ 3FilePrograms/first3FileDemo/main3File.cpp
@@ -0,0 +1,8 @@
+
+#include "header.h"
+
+int main() {
+    cout << "main function" << endl;
+    myFuncPrints();
+    cout << "done in main function" << endl;
+}
